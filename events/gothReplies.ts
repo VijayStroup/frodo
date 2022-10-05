d06b01fbd5516c6f2190d3fa8108917f254b428f
Adrian Hossen <adrianhossen5@gmail.com>
2022-10-04 20:45:58 -0400

    Goth

--- events/gothReplies.ts
+++ events/gothReplies.ts
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import type { Message, TextChannel } from 'discord.js'
 
 const GothReplies = {
@@ -29,3 +30,36 @@ const GothReplies = {
 }
 
 export default GothReplies
+=======
+import type { Message, TextChannel } from 'discord.js'
+
+const GothReplies = {
+  name: 'messageCreate',
+
+  async execute(message: Message) {
+    if (message.author.bot) return
+
+    const nsfwChannel = await message.guild?.channels.cache.find(
+        channel => channel.name === '🔞｜nsfw'
+      ) as TextChannel
+
+    const coin = (Math.floor(Math.random() * 2) == 0)
+
+    if (message.channel === nsfwChannel && coin)
+    {
+        const loweredMessage = message.content.toLowerCase()
+        const fetch = require('node-fetch');
+    
+        const url = `https://g.tenor.com/v1/search?q=${loweredMessage + " girl"}&key=CQ2A3QONWOOL`
+        const response = await fetch(url);
+        const json = await response.json();
+        const index = Math.floor(Math.random() * json.results.length);
+    
+        if (loweredMessage.split(' ')[0] === 'goth')
+          await message.reply(json.results[index].url)
+    }
+  }
+}
+
+export default GothReplies
+>>>>>>> cbe9d10eb98e7e9ff587b72f68584c95e28f973c
