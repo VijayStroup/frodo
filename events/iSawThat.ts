72ac5ec2669d12c9d5dc6183e7dd8a289951ceaa
Vijay Stroup <vijaystroup@gmail.com>
2022-11-10 16:12:06 -0500

    iSawThat to just send i saw that

--- events/iSawThat.ts
+++ events/iSawThat.ts
@@ -1,30 +1,13 @@
-import type { Message, TextChannel } from 'discord.js'
-import { MessageEmbed } from 'discord.js'
-import colors from '../utils/colors'
+import type { Message } from 'discord.js'
+
+const ignored = ['864311867765686332', '536716152680873988']
 
 const ISawThat = {
   name: 'messageDelete',
   async execute(message: Message) {
     if (message.author.bot) return
+    if (ignored.includes(message.author.id)) return
 
-    const logChannel = await message.guild.channels.cache.find(
-      channel => channel.name === 'logs'
-    ) as TextChannel
-
-    if (!logChannel) {
-      await message.channel.send(`i saw that <@${message.author.id}>`)
-      return
-    }
-
-    const deleteEmbed = new MessageEmbed()
-      .setColor(colors.red)
-      .setTitle(`Message Delete`)
-      .setDescription(`<@${message.author.id}> <#${message.channel.id}>\n` + message.content.slice(0, 4096 - 44)) // 44 offset for author and channel
-      .addFields(message.attachments.map((attchment) => ({ name: 'Attachment', value: attchment.url, inline: false })))
-      .setImage(message.attachments.first()?.url)
-      .setTimestamp()
-
-    await logChannel.send({ embeds: [deleteEmbed] })
     await message.channel.send(`i saw that <@${message.author.id}>`)
   }
 }
