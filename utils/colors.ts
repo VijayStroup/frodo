4d281ef936cc6c479138cceb46ea95d6e63c8f89
Vijay Stroup <vijaystroup@gmail.com>
2022-11-10 16:10:20 -0500

    add red color

--- utils/colors.ts
+++ utils/colors.ts
@@ -2,7 +2,8 @@ import type { ColorResolvable } from "discord.js"
 
 const colors: { [key: string]: ColorResolvable } = {
   gold: '#FFD940',
-  blue: '#4599FF'
+  blue: '#4599FF',
+  red: '#FF4D4D',
 }
 
 export default colors
